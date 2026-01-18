c31666bb1b2357a9a2247300a2de2a9b8a289417
Alexandre Agneray <agneraya@gmail.com>
2026-01-18 11:02:47 +0100

    Ajout de la connexion à un utilisateur, et envoie de message en direct à un autre  user

--- main.c
+++ main.c
@@ -13,15 +13,19 @@
 
 #define USERID_LEN 4
 #define USERNAME_LEN 64
+#define USERMSG_MAXLEN 4096
+#define SRVMSG_MAXLEN 8192
 
 #define USER_WELCOME_MSG "Bienvenue sur le serveur de discussion, quel est votre nom ?\n"
 #define USER_WELCOME_MSG_LEN sizeof(USER_WELCOME_MSG)
+
 
 typedef struct UserInfo
 {
     int used; // 0 si inutilisé, 1 si user actif
     int authentified; // 0 si non authentifié, 1 si authentifié
     char username[USERNAME_LEN];
+    int speakto; // indice de l'interlocuteur
 } UserInfo;
 
 /**
@@ -53,7 +57,7 @@ int findEmptySlot(int tab[], unsigned int tabSize)
  * @param nUserInfoLen 
  * @param ppzUserList 
  */
-void getUserListString(struct UserInfo atUserInfo[], unsigned int nUserInfoLen, char **ppzUserList)
+int getUserListString(struct UserInfo atUserInfo[], unsigned int nUserInfoLen, char **ppzUserList)
 {
     const unsigned int USER_LABEL_SIZE = USERID_LEN + USERNAME_LEN + 3; // userid - USERNAME
     *ppzUserList = malloc(USER_LABEL_SIZE * nUserInfoLen);
@@ -62,9 +66,10 @@ void getUserListString(struct UserInfo atUserInfo[], unsigned int nUserInfoLen, 
     {
         if(atUserInfo[i].used)
         {
-            offset += sprintf((*ppzUserList)+offset, "%s\n", atUserInfo[i].username);
+            offset += sprintf((*ppzUserList)+offset, "%d - %s\n", i,atUserInfo[i].username);
         }
     }
+    return offset;
 }
 
 /**
@@ -84,8 +89,31 @@ void freeUserListString(char **ppzUserList)
     
 }
 
+/**
+ * @brief Renvoie l'indice du fd de socket.
+ * 
+ * @param sockfd socket recherchée
+ * @param aSockfd liste des sockets
+ * @param sockfdLen taille de la liste 
+ * @return int 
+ */
+int getIdxFromSocketfd(int sockfd, int aSockfd[], int sockfdLen)
+{
+    for(int i=0; i<sockfdLen;++i)
+    {
+        if(aSockfd[i]==sockfd)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
 int main(int argc, char **argv)
 {
+
+    // -----------------------------------------------------------------------------------
     int opt;
     char *pzPort = NULL;
     int sockfd;
@@ -106,6 +134,9 @@ int main(int argc, char **argv)
 
     struct UserInfo atUserInfo[USER_COUNT];
     memset(atUserInfo, 0, sizeof(atUserInfo));
+
+    struct UserInMail* atUserInMail[USER_COUNT];
+    memset(atUserInMail, 0, sizeof(atUserInMail));
     
 
     struct epoll_event ev;
@@ -117,6 +148,9 @@ int main(int argc, char **argv)
     char buffer[BUFFER_SIZE]; 
     ssize_t nBytes;
 
+    char bufferToSend[SRVMSG_MAXLEN];
+    memset(bufferToSend,0,SRVMSG_MAXLEN);
+
     const char * QUIT = "QUIT";
 
     int epfd = epoll_create(11);
@@ -127,7 +161,7 @@ int main(int argc, char **argv)
     }
 
     int n;
-
+    // -----------------------------------------------------------------------------------
 
 
     while((opt = getopt(argc,argv,"p:h"))!=-1)
@@ -203,7 +237,6 @@ int main(int argc, char **argv)
 
         for(int i=0;i<n;i++)
         {
-            // printf("%d",i);
             if(events[i].data.fd==sockfd)
             {
                 printf("Un nouvel utiisateur veut se connecter !\n");
@@ -237,12 +270,47 @@ int main(int argc, char **argv)
                 }
                 printf("%lu octets recus\n", nBytes);
 
+                int idx = getIdxFromSocketfd(events[i].data.fd, csockfd, USER_COUNT);
 
-                // fwrite(buffer,1,nBytes,stdout);
-                if(!memcmp(QUIT, buffer, strlen(QUIT)-1))
+                if(!atUserInfo[idx].authentified) // authentification
                 {
-                    printf("ON QUITTE\n");
+                    strncpy(atUserInfo[idx].username, buffer, USERNAME_LEN);
+                    atUserInfo[idx].authentified = 1;
+                    atUserInfo[idx].speakto = -1;
+                    char *pzUserList;
+                    int nUserListLen = 0;
+                    nUserListLen = getUserListString(atUserInfo, USER_COUNT, &pzUserList);
+                    send(events[i].data.fd, pzUserList, nUserListLen,0);
+                    freeUserListString(&pzUserList);
                 }
+                else if(atUserInfo[idx].authentified &&
+                    atUserInfo[idx].speakto == -1) // connexion à un autre user
+                {
+                    unsigned int speakToIdx;
+                    int ret;
+                    ret = sscanf(buffer,"%u",&speakToIdx);
+                    if(ret!=EOF)
+                    {
+                        if(speakToIdx < USER_COUNT)
+                        {
+                            memset(buffer, 0, BUFFER_SIZE);
+                            nBytes = snprintf(buffer, BUFFER_SIZE, "Vous parlez maintenant à %s\n", atUserInfo[speakToIdx].username);
+                            atUserInfo[idx].speakto = speakToIdx;
+                            send(csockfd[idx],buffer,nBytes,0);
+                        }
+                    }
+                }
+                else // user authentifié et connecté à un autre user
+                {
+                    const unsigned int speakto = atUserInfo[idx].speakto;
+                    send(csockfd[speakto], buffer, nBytes,0);
+
+                    if(!memcmp(QUIT, buffer, strlen(QUIT)-1))
+                    {
+                        printf("ON QUITTE\n");
+                    }
+                }
+
             }
         }
     }
