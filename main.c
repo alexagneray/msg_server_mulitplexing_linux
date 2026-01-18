3398e3f915e3dff29a8eb03f220d332072491720
Alexandre Agneray <agneraya@gmail.com>
2026-01-18 14:00:44 +0100

    modif convention de nommage

--- main.c
+++ main.c
@@ -22,25 +22,25 @@
 
 typedef struct
 {
-    int used; // 0 si inutilisé, 1 si user actif
-    int authentified; // 0 si non authentifié, 1 si authentifié
-    char username[USERNAME_LEN];
-    int speakto; // indice de l'interlocuteur
+    int b_used; // 0 si inutilisé, 1 si user actif
+    int b_authentified; // 0 si non authentifié, 1 si authentifié
+    char ac_username[USERNAME_LEN];
+    int idx_speakto; // indice de l'interlocuteur
 } user_info_t;
 
 /**
  * @brief Renvoie l'indice de la première valeur -1 du tableau.
  * Renvoie -1 si la valeur -1 n'est pas dans le tableau
  * 
- * @param tab 
- * @param tabSize 
+ * @param ac_tocheck 
+ * @param u_tab_size 
  * @return int 
  */
-int find_empty_slot(int tab[], unsigned int tabSize)
+int find_empty_slot(int ac_tocheck[], unsigned int u_tab_size)
 {
-    for(unsigned int i=0;i<tabSize;++i)
+    for(unsigned int i=0;i<u_tab_size;++i)
     {
-        if(tab[i]==-1)
+        if(ac_tocheck[i]==-1)
         {
             return i;
         }
@@ -50,23 +50,23 @@ int find_empty_slot(int tab[], unsigned int tabSize)
 
 /**
  * @brief Génère un string contenant la liste des utilisateurs actifs. 
- * La fonction alloue dynamiquement et mets à jour le pointeur ppzUserList.
+ * La fonction alloue dynamiquement et mets à jour le pointeur ppz_user_list.
  * La mémoire allouée devra être libérée avec free_user_list_string()
  * 
- * @param atUserInfo 
- * @param nUserInfoLen 
- * @param ppzUserList 
+ * @param at_userinfo 
+ * @param cnt_userinfo 
+ * @param ppz_user_list 
  */
-int get_user_list_string(user_info_t atUserInfo[], unsigned int nUserInfoLen, char **ppzUserList)
+int get_user_list_string(user_info_t at_userinfo[], unsigned int cnt_userinfo, char **ppz_user_list)
 {
     const unsigned int USER_LABEL_SIZE = USERID_LEN + USERNAME_LEN + 3; // userid - USERNAME
-    *ppzUserList = malloc(USER_LABEL_SIZE * nUserInfoLen);
+    *ppz_user_list = malloc(USER_LABEL_SIZE * cnt_userinfo);
     int offset = 0;
-    for(unsigned int i=0;i<nUserInfoLen;++i)
+    for(unsigned int i=0;i<cnt_userinfo;++i)
     {
-        if(atUserInfo[i].used)
+        if(at_userinfo[i].b_used)
         {
-            offset += sprintf((*ppzUserList)+offset, "%d - %s\n", i,atUserInfo[i].username);
+            offset += sprintf((*ppz_user_list)+offset, "%d - %s\n", i,at_userinfo[i].ac_username);
         }
     }
     return offset;
@@ -77,14 +77,14 @@ int get_user_list_string(user_info_t atUserInfo[], unsigned int nUserInfoLen, ch
  * Cette fonction doit impérativement être appelée avec get_user_list_string
  * pour libérer la mémoire allouée dynamiquement !
  * 
- * @param ppzUserList 
+ * @param ppz_user_list 
  */
-void free_user_list_string(char **ppzUserList)
+void free_user_list_string(char **ppz_user_list)
 {
-    if(*ppzUserList)
+    if(*ppz_user_list)
     {
-        free(*ppzUserList);
-        *ppzUserList = NULL;
+        free(*ppz_user_list);
+        *ppz_user_list = NULL;
     }
     
 }
@@ -92,16 +92,16 @@ void free_user_list_string(char **ppzUserList)
 /**
  * @brief Renvoie l'indice du fd de socket.
  * 
- * @param sockfd socket recherchée
- * @param aSockfd liste des sockets
- * @param sockfdLen taille de la liste 
+ * @param fd_sock socket recherchée
+ * @param afd_sock liste des sockets
+ * @param cnt_fd_sock nombre de sockets
  * @return int 
  */
-int get_idx_from_sockfd(int sockfd, int aSockfd[], int sockfdLen)
+int get_idx_from_sockfd(int fd_sock, int afd_sock[], unsigned int cnt_fd_sock)
 {
-    for(int i=0; i<sockfdLen;++i)
+    for(unsigned int i=0; i<cnt_fd_sock;++i)
     {
-        if(aSockfd[i]==sockfd)
+        if(afd_sock[i]==fd_sock)
         {
             return i;
         }
@@ -114,59 +114,59 @@ int main(int argc, char **argv)
 {
 
     // ------------------------------------------------------------------------
-    int opt;
-    char *pzPort = NULL;
-    int sockfd;
-    int err;
+    int i_opt;
+    char *pz_port = NULL;
+    int fd_sock;
+    int i_err;
 
-    struct addrinfo hints;
-    struct addrinfo *pRes;
+    struct addrinfo t_hints;
+    struct addrinfo *pt_res;
 
-    struct sockaddr cltSockAddr;
-    socklen_t cltSockAddrLen;
+    struct sockaddr t_client_sockaddr;
+    socklen_t cnt_client_sockaddr;
 
     const unsigned int USER_COUNT = 10;
-    int csockfd[USER_COUNT];
+    int afd_user_sock[USER_COUNT];
     for(unsigned int i=0;i<USER_COUNT;++i)
     {
-        csockfd[i] = -1;
+        afd_user_sock[i] = -1;
     }
 
-    user_info_t atUserInfo[USER_COUNT];
-    memset(atUserInfo, 0, sizeof(atUserInfo));
+    user_info_t at_userinfo[USER_COUNT];
+    memset(at_userinfo, 0, sizeof(at_userinfo));
     
 
-    struct epoll_event ev;
+    struct epoll_event t_event;
 
-    struct epoll_event events[USER_COUNT+1];
+    struct epoll_event at_user_events[USER_COUNT+1];
 
 
     const unsigned int BUFFER_SIZE = 256;
     char buffer[BUFFER_SIZE]; 
-    ssize_t nBytes;
+    ssize_t cnt_bytes_transfered;
 
-    char bufferToSend[SRVMSG_MAXLEN];
-    memset(bufferToSend,0,SRVMSG_MAXLEN);
+    char ac_tosend[SRVMSG_MAXLEN];
+    memset(ac_tosend,0,SRVMSG_MAXLEN);
 
-    const char * QUIT = "QUIT";
+    const char *QUIT = "QUIT";
 
-    int epfd = epoll_create(11);
-    if(epfd < 0)
+    int fd_ep = epoll_create(11);
+    if(fd_ep < 0)
     {
         perror("epoll_create");
         exit(EXIT_FAILURE);
     }
 
-    int n;
+    int cnt_user_events;
     // ------------------------------------------------------------------------
 
 
-    while((opt = getopt(argc,argv,"p:h"))!=-1)
+    while((i_opt = getopt(argc,argv,"p:h"))!=-1)
     {
-        switch (opt)
+        switch (i_opt)
         {
         case 'p':
-            pzPort = optarg;
+            pz_port = optarg;
             break;
         case 'h':
             printf("%s -a address -p port\n", argv[0]);
@@ -176,70 +176,70 @@ int main(int argc, char **argv)
         }
     }
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
+    memset(&t_hints, 0, sizeof(t_hints));
+    t_hints.ai_family = AF_INET;
+    t_hints.ai_socktype = SOCK_STREAM;
+    t_hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
 
-    err = getaddrinfo(NULL, pzPort, &hints, &pRes);
-    if(err == -1)
+    i_err = getaddrinfo(NULL, pz_port, &t_hints, &pt_res);
+    if(i_err == -1)
     {
-        fprintf(stderr, "%s\n", gai_strerror(err));
+        fprintf(stderr, "%s\n", gai_strerror(i_err));
         exit(EXIT_FAILURE);
     }
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    fd_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    ev.data.fd = sockfd;
-    ev.events = EPOLLIN;
-    err = epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
-    fcntl(sockfd, F_SETFL, O_NONBLOCK);
+    t_event.data.fd = fd_sock;
+    t_event.events = EPOLLIN;
+    i_err = epoll_ctl(fd_ep, EPOLL_CTL_ADD, fd_sock, &t_event);
+    fcntl(fd_sock, F_SETFL, O_NONBLOCK);
 
-    if(err == -1)
+    if(i_err == -1)
     {
-        shutdown(sockfd, SHUT_RDWR);
-        close(sockfd);
+        shutdown(fd_sock, SHUT_RDWR);
+        close(fd_sock);
         perror("epoll_ctl");
         exit(EXIT_FAILURE);
     }
 
-    err = bind(sockfd, pRes->ai_addr, pRes->ai_addrlen);
-    if(err == -1)
+    i_err = bind(fd_sock, pt_res->ai_addr, pt_res->ai_addrlen);
+    if(i_err == -1)
     {
-        shutdown(sockfd, SHUT_RDWR);
-        close(sockfd);
+        shutdown(fd_sock, SHUT_RDWR);
+        close(fd_sock);
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    err = listen(sockfd, 10);
-    if(err == -1)
+    i_err = listen(fd_sock, 10);
+    if(i_err == -1)
     {
-        shutdown(sockfd, SHUT_RDWR);
-        close(sockfd);
+        shutdown(fd_sock, SHUT_RDWR);
+        close(fd_sock);
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    printf("Initialisation du serveur sur le port : %s Achevée\n", pzPort);
+    printf("Initialisation du serveur sur le port : %s Achevée\n", pz_port);
 
     while(1)
     {   
-        n = epoll_wait(epfd,events,USER_COUNT+1,-1);
+        cnt_user_events = epoll_wait(fd_ep,at_user_events,USER_COUNT+1,-1);
 
-        if(n==-1)
+        if(cnt_user_events==-1)
         {
             perror("epoll_wait");
             exit(EXIT_FAILURE);
         }
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<cnt_user_events;i++)
         {
-            if(events[i].data.fd==sockfd)
+            if(at_user_events[i].data.fd==fd_sock)
             {
                 printf("Un nouvel utiisateur veut se connecter !\n");
                 int newsock;
-                newsock = accept(sockfd,&cltSockAddr,&cltSockAddrLen);
-                int idx = find_empty_slot(csockfd, USER_COUNT);
+                newsock = accept(fd_sock,&t_client_sockaddr,&cnt_client_sockaddr);
+                int idx = find_empty_slot(afd_user_sock, USER_COUNT);
                 if(idx==-1) // Il n'y a plus de slot dispo, on ferme la connexion
                 {
                     close(newsock);
@@ -248,12 +248,12 @@ int main(int argc, char **argv)
                 {
                     printf("Ajout d'un nouvel utiisateur à l'emplacement %d\n",
                              idx);
-                    csockfd[idx] = newsock;
-                    ev.data.fd = newsock;
-                    ev.events = EPOLLIN;
-                    memset(atUserInfo + idx,0,sizeof(user_info_t));
-                    atUserInfo[idx].used = 1;
-                    epoll_ctl(epfd, EPOLL_CTL_ADD, csockfd[idx], &ev);
+                    afd_user_sock[idx] = newsock;
+                    t_event.data.fd = newsock;
+                    t_event.events = EPOLLIN;
+                    memset(at_userinfo + idx,0,sizeof(user_info_t));
+                    at_userinfo[idx].b_used = 1;
+                    epoll_ctl(fd_ep, EPOLL_CTL_ADD, afd_user_sock[idx], &t_event);
 
                     send(newsock, USER_WELCOME_MSG, USER_WELCOME_MSG_LEN,0);
                 }
@@ -261,28 +261,28 @@ int main(int argc, char **argv)
             else // On parle avec un user connecté
             {
                 memset(buffer, 0, BUFFER_SIZE);
-                nBytes = recv(events[i].data.fd,buffer,BUFFER_SIZE,0);
-                if(nBytes==0)
+                cnt_bytes_transfered = recv(at_user_events[i].data.fd,buffer,BUFFER_SIZE,0);
+                if(cnt_bytes_transfered==0)
                 {
                     continue;
                 }
-                printf("%lu octets recus\n", nBytes);
+                printf("%lu octets recus\n", cnt_bytes_transfered);
 
-                int idx = get_idx_from_sockfd(events[i].data.fd, csockfd, USER_COUNT);
+                int idx = get_idx_from_sockfd(at_user_events[i].data.fd, afd_user_sock, USER_COUNT);
 
-                if(!atUserInfo[idx].authentified) // authentification
+                if(!at_userinfo[idx].b_authentified) // authentification
                 {
-                    strncpy(atUserInfo[idx].username, buffer, USERNAME_LEN);
-                    atUserInfo[idx].authentified = 1;
-                    atUserInfo[idx].speakto = -1;
+                    strncpy(at_userinfo[idx].ac_username, buffer, USERNAME_LEN);
+                    at_userinfo[idx].b_authentified = 1;
+                    at_userinfo[idx].idx_speakto = -1;
                     char *pzUserList;
                     int nUserListLen = 0;
-                    nUserListLen = get_user_list_string(atUserInfo, USER_COUNT, &pzUserList);
-                    send(events[i].data.fd, pzUserList, nUserListLen,0);
+                    nUserListLen = get_user_list_string(at_userinfo, USER_COUNT, &pzUserList);
+                    send(at_user_events[i].data.fd, pzUserList, nUserListLen,0);
                     free_user_list_string(&pzUserList);
                 }
-                else if(atUserInfo[idx].authentified &&
-                    atUserInfo[idx].speakto == -1) // connexion à un autre user
+                else if(at_userinfo[idx].b_authentified &&
+                    at_userinfo[idx].idx_speakto == -1) // connexion à un autre user
                 {
                     unsigned int speakToIdx;
                     int ret;
@@ -292,18 +292,18 @@ int main(int argc, char **argv)
                         if(speakToIdx < USER_COUNT)
                         {
                             memset(buffer, 0, BUFFER_SIZE);
-                            nBytes = snprintf(buffer, BUFFER_SIZE,
+                            cnt_bytes_transfered = snprintf(buffer, BUFFER_SIZE,
                                                 "Vous parlez maintenant à %s\n",
-                                                atUserInfo[speakToIdx].username);
-                            atUserInfo[idx].speakto = speakToIdx;
-                            send(csockfd[idx],buffer,nBytes,0);
+                                                at_userinfo[speakToIdx].ac_username);
+                            at_userinfo[idx].idx_speakto = speakToIdx;
+                            send(afd_user_sock[idx],buffer,cnt_bytes_transfered,0);
                         }
                     }
                 }
                 else // user authentifié et connecté à un autre user
                 {
-                    const unsigned int speakto = atUserInfo[idx].speakto;
-                    send(csockfd[speakto], buffer, nBytes,0);
+                    const unsigned int idx_speakto = at_userinfo[idx].idx_speakto;
+                    send(afd_user_sock[idx_speakto], buffer, cnt_bytes_transfered,0);
 
                     if(!memcmp(QUIT, buffer, strlen(QUIT)-1))
                     {
@@ -315,8 +315,8 @@ int main(int argc, char **argv)
         }
     }
 
-    shutdown(sockfd, SHUT_RDWR);
-    close(sockfd);
+    shutdown(fd_sock, SHUT_RDWR);
+    close(fd_sock);
     
     exit(EXIT_SUCCESS);
 }
